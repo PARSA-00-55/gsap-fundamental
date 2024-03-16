95b76d06a57ff2be6a8d16b6e2f3262b6d6e44b5
PARSA <parsan.a360@gmail.com>
2024-03-16 16:38:58 +0600

    GsapScrollTrigger

--- src/pages/GsapScrollTrigger.jsx
+++ src/pages/GsapScrollTrigger.jsx
@@ -1,10 +1,35 @@
 import gsap from "gsap";
-import { ScrollTrigger } from "gsap/all";
 import { useRef } from "react";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const GsapScrollTrigger = () => {
   const scrollRef = useRef();
   // TODO: Implement the gsap scroll trigger
+  useGSAP(
+    () => {
+      const boxes = gsap.utils.toArray(scrollRef.current.children);
+
+      boxes.forEach((box) => {
+        gsap.to(box, {
+          x: 150 * (boxes.indexOf(box) + 5),
+          rotation: 360,
+          borderRadius: "100%",
+          scale: 1.5,
+          scrollTrigger: {
+            trigger: box,
+            start: "bottom bottom",
+            end: "top 10%",
+            scrub: true,
+          },
+          ease: "power1.inOut",
+        });
+      });
+    },
+    { scope: scrollRef }
+  );
 
   return (
     <main>
